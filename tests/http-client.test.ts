61f60b3dfc513d25d7d094c87915c1182077ea73
Raghu Rana <raghurana@gmail.com>
2025-07-03 21:43:17 +1000

    get is covered comprehensively

--- tests/http-client.test.ts
+++ tests/http-client.test.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import { HttpClient } from '../src/http-client';
 
 describe('HttpClient', () => {
@@ -15,6 +16,41 @@ describe('HttpClient', () => {
       id: 1,
       title: expect.any(String),
       body: expect.any(String),
+    });
+  });
+
+  test('should return 404 when fetching a non-existent post', async () => {
+    const httpClient = new HttpClient(BASE_URL);
+    const response = await httpClient.get('/posts/-1');
+    expect(response.status).toBe(404);
+    expect(response.headers).toMatchObject({
+      'content-type': expect.stringContaining('application/json'),
+    });
+    expect(response.status).toBe(404);
+    expect(response.headers).toMatchObject({
+      'content-type': expect.stringContaining('application/json'),
+    });
+    expect(response.data).toBeUndefined();
+    expect(response.error).toBeInstanceOf(AxiosError);
+    const axiosError = response.error as AxiosError;
+    expect(axiosError.toJSON()).toMatchObject({
+      status: 404,
+      code: 'ERR_BAD_REQUEST',
+      message: 'Request failed with status code 404',
+    });
+  });
+
+  test('when a wrong url is provided, it should throw an error', async () => {
+    const httpClient = new HttpClient('https://jsonplaceholder.XXX.com');
+    const response = await httpClient.get('/posts/1');
+    expect(response.status).toBe(500);
+    expect(response.headers).toMatchObject({});
+    expect(response.error).toBeInstanceOf(AxiosError);
+    const axiosError = response.error as AxiosError;
+    expect(axiosError.toJSON()).toMatchObject({
+      status: undefined,
+      code: 'EPROTO',
+      message: expect.any(String),
     });
   });
 });
